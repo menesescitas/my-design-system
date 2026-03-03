eeedffdc03ba6b0f8e31fc892636e2ca5183dc41
menesescitas <48730283+menesescitas@users.noreply.github.com>
2026-03-02 20:34:48 -0500

    Update color configuration to use global tokens
    Replaced custom color mapping with tokens from Style Dictionary.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,13 @@
+// Importamos el archivo JS que genera Style Dictionary
+const tokens = require('./build/js/tailwind-tokens.js');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-      colors: {
-        // Aquí mapeamos tus tokens de Figma
-        'pds-purple': {
-          50: '#E7DDF8',
-          300: '#AB88E7',
-          500: '#6429CD',
-          700: '#330072',
-          900: '#1F0D3F',
-        },
-        // Cuando agregues más en el JSON, los sumas aquí
-      },
+      // Esto mapea automáticamente TODO lo que esté bajo "global" en tu JSON
+      colors: tokens.global, 
     },
   },
   plugins: [],
