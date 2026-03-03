340e0fbb0d16ce4a01f7ea3de8f844de065fa75e
GitHub Action <action@github.com>
2026-03-03 01:32:04 +0000

    style: update generated tokens

---
+++ build/js/tailwind-tokens.js
@@ -0,0 +1,252 @@
+/**
+ * Do not edit directly, this file was auto-generated.
+ */
+
+module.exports = {
+  global: {
+    "pds-color-primary-purple-50": {
+      $type: "color",
+      $value: "#e7ddf8",
+      filePath: "tokens.json",
+      isSource: true,
+      original: {
+        $type: "color",
+        $value: "#E7DDF8",
+      },
+      name: "GlobalPdsColorPrimaryPurple50",
+      attributes: {
+        category: "global",
+        type: "pds-color-primary-purple-50",
+      },
+      path: ["global", "pds-color-primary-purple-50"],
+    },
+    "pds-color-primary-purple-300": {
+      $type: "color",
+      $value: "#ab88e7",
+      filePath: "tokens.json",
+      isSource: true,
+      original: {
+        $type: "color",
+        $value: "#AB88E7",
+      },
+      name: "GlobalPdsColorPrimaryPurple300",
+      attributes: {
+        category: "global",
+        type: "pds-color-primary-purple-300",
+      },
+      path: ["global", "pds-color-primary-purple-300"],
+    },
+    "pds-color-primary-purple-500": {
+      $type: "color",
+      $value: "#6429cd",
+      filePath: "tokens.json",
+      isSource: true,
+      original: {
+        $type: "color",
+        $value: "#6429CD",
+      },
+      name: "GlobalPdsColorPrimaryPurple500",
+      attributes: {
+        category: "global",
+        type: "pds-color-primary-purple-500",
+      },
+      path: ["global", "pds-color-primary-purple-500"],
+    },
+    "pds-color-primary-purple-700": {
+      $type: "color",
+      $value: "#330072",
+      filePath: "tokens.json",
+      isSource: true,
+      original: {
+        $type: "color",
+        $value: "#330072",
+      },
+      name: "GlobalPdsColorPrimaryPurple700",
+      attributes: {
+        category: "global",
+        type: "pds-color-primary-purple-700",
+      },
+      path: ["global", "pds-color-primary-purple-700"],
+    },
+    "pds-color-primary-purple-900": {
+      $type: "color",
+      $value: "#1f0d3f",
+      filePath: "tokens.json",
+      isSource: true,
+      original: {
+        $type: "color",
+        $value: "#1F0D3F",
+      },
+      name: "GlobalPdsColorPrimaryPurple900",
+      attributes: {
+        category: "global",
+        type: "pds-color-primary-purple-900",
+      },
+      path: ["global", "pds-color-primary-purple-900"],
+    },
+    "pds-color-primary-gray-50": {
+      $type: "color",
+      $value: "#d8d8d6",
+      filePath: "tokens.json",
+      isSource: true,
+      original: {
+        $type: "color",
+        $value: "#D8D8D6",
+      },
+      name: "GlobalPdsColorPrimaryGray50",
+      attributes: {
+        category: "global",
+        type: "pds-color-primary-gray-50",
+      },
+      path: ["global", "pds-color-primary-gray-50"],
+    },
+    "pds-color-primary-gray-300": {
+      $type: "color",
+      $value: "#75756d",
+      filePath: "tokens.json",
+      isSource: true,
+      original: {
+        $type: "color",
+        $value: "#75756D",
+      },
+      name: "GlobalPdsColorPrimaryGray300",
+      attributes: {
+        category: "global",
+        type: "pds-color-primary-gray-300",
+      },
+      path: ["global", "pds-color-primary-gray-300"],
+    },
+    "pds-color-primary-gray-500": {
+      $type: "color",
+      $value: "#1d1d1b",
+      filePath: "tokens.json",
+      isSource: true,
+      original: {
+        $type: "color",
+        $value: "#1D1D1B",
+      },
+      name: "GlobalPdsColorPrimaryGray500",
+      attributes: {
+        category: "global",
+        type: "pds-color-primary-gray-500",
+      },
+      path: ["global", "pds-color-primary-gray-500"],
+    },
+    "pds-color-primary-gray-700": {
+      $type: "color",
+      $value: "#141413",
+      filePath: "tokens.json",
+      isSource: true,
+      original: {
+        $type: "color",
+        $value: "#141413",
+      },
+      name: "GlobalPdsColorPrimaryGray700",
+      attributes: {
+        category: "global",
+        type: "pds-color-primary-gray-700",
+      },
+      path: ["global", "pds-color-primary-gray-700"],
+    },
+    "pds-color-primary-gray-900": {
+      $type: "color",
+      $value: "#030302",
+      filePath: "tokens.json",
+      isSource: true,
+      original: {
+        $type: "color",
+        $value: "#030302",
+      },
+      name: "GlobalPdsColorPrimaryGray900",
+      attributes: {
+        category: "global",
+        type: "pds-color-primary-gray-900",
+      },
+      path: ["global", "pds-color-primary-gray-900"],
+    },
+    "pds-color-primary-indigo-50": {
+      $type: "color",
+      $value: "#f6f6fc",
+      filePath: "tokens.json",
+      isSource: true,
+      original: {
+        $type: "color",
+        $value: "#F6F6FC",
+      },
+      name: "GlobalPdsColorPrimaryIndigo50",
+      attributes: {
+        category: "global",
+        type: "pds-color-primary-indigo-50",
+      },
+      path: ["global", "pds-color-primary-indigo-50"],
+    },
+    "pds-color-primary-indigo-300": {
+      $type: "color",
+      $value: "#dfdfff",
+      filePath: "tokens.json",
+      isSource: true,
+      original: {
+        $type: "color",
+        $value: "#DFDFFF",
+      },
+      name: "GlobalPdsColorPrimaryIndigo300",
+      attributes: {
+        category: "global",
+        type: "pds-color-primary-indigo-300",
+      },
+      path: ["global", "pds-color-primary-indigo-300"],
+    },
+    "pds-color-primary-indigo-500": {
+      $type: "color",
+      $value: "#bdbdff",
+      filePath: "tokens.json",
+      isSource: true,
+      original: {
+        $type: "color",
+        $value: "#BDBDFF",
+      },
+      name: "GlobalPdsColorPrimaryIndigo500",
+      attributes: {
+        category: "global",
+        type: "pds-color-primary-indigo-500",
+      },
+      path: ["global", "pds-color-primary-indigo-500"],
+    },
+    "pds-color-primary-indigo-700": {
+      $type: "color",
+      $value: "#8a8aff",
+      filePath: "tokens.json",
+      isSource: true,
+      original: {
+        $type: "color",
+        $value: "#8A8AFF",
+      },
+      name: "GlobalPdsColorPrimaryIndigo700",
+      attributes: {
+        category: "global",
+        type: "pds-color-primary-indigo-700",
+      },
+      path: ["global", "pds-color-primary-indigo-700"],
+    },
+    "pds-color-primary-indigo-900": {
+      $type: "color",
+      $value: "#4646ff",
+      filePath: "tokens.json",
+      isSource: true,
+      original: {
+        $type: "color",
+        $value: "#4646FF",
+      },
+      name: "GlobalPdsColorPrimaryIndigo900",
+      attributes: {
+        category: "global",
+        type: "pds-color-primary-indigo-900",
+      },
+      path: ["global", "pds-color-primary-indigo-900"],
+    },
+  },
+  $themes: [],
+  $metadata: {
+    tokenSetOrder: ["global"],
+  },
+};
